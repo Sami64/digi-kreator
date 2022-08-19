04f0e041ef883f873df3fa1d77389776ac167802
Samuel Kow Paintsil <smashbros54@gmail.com>
2022-08-19 11:25:16 +0000

    🚀 set layouts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { NextPage } from "next"
+import type { AppProps } from "next/app"
+import { ReactElement, ReactNode } from "react"
+import "../styles/globals.css"
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export type NextPageWithLayout = NextPage & {
+	getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+	Component: NextPageWithLayout
+}
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+	const getLayout = Component.getLayout ?? ((page) => page)
+	return <Component {...pageProps} />
 }
 
 export default MyApp
