a6ac07dd1c776c95a23c0fbf4022e63d4aac9d31
Samuel Kow Paintsil <smashbros54@gmail.com>
2022-08-29 00:09:03 +0000

    ⚡️ for streak

--- pages/kreator/jobs.tsx
+++ pages/kreator/jobs.tsx
@@ -25,7 +25,7 @@ const Jobs: NextPageWithLayout = () => {
 						<div className="flex flex-col bg-white p-5 rounded-2xl shadow-lg items-center justify-center">
 							<h1 className="text-lg capitalize font-extrabold">{job.title}</h1>
 							<button className="hover:shadow-xl bg-slate-800 text-white px-5 py-2 rounded-lg uppercase font-bold text-lg mt-2">
-								view job
+								view job for streak
 							</button>
 						</div>
 					))}
